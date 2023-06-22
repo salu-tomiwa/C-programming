f791378a3916c8d0c054da081df1a9f9bff64819
salu-tomiwa01~ <saluolaoluwatomiwa@gmail.com>
2023-06-22 18:23:20 +0100

    variables

---
+++ VARIABLES/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+  int num1 = 5, num2 = 10;
+
+  //squares of numbers
+  int squares1 = num1 * num1, squares2 = num2 * num2;
+
+  //addition of squares
+  int result = squares1 + squares2;
+  printf ("%i", result);
+}
